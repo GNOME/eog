6262b1c2851ce4a99b24a41f58bb60fd4d87916d
Arturo Espinosa <unammx@src.gnome.org>
1999-09-27 19:14:06 +0000

    Sync to desktop - Federico

---
+++ src/gnome-list-view.h
@@ -0,0 +1,69 @@
+/* GNOME libraries - abstract list view
+ *
+ * Copyright (C) 1999 The Free Software Foundation
+ *
+ * Author: Federico Mena-Quintero <federico@gimp.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef GNOME_LIST_VIEW_H
+#define GNOME_LIST_VIEW_H
+
+#include <libgnome/gnome-defs.h>
+#include <libgnomeui/gnome-canvas.h>
+#include "gnome-item-view-factory.h"
+#include "gnome-list-model.h"
+
+BEGIN_GNOME_DECLS
+
+
+
+#define GNOME_TYPE_LIST_VIEW            (gnome_list_view_get_type ())
+#define GNOME_LIST_VIEW(obj)            (GTK_CHECK_CAST ((obj),		\
+					 GNOME_TYPE_LIST_VIEW, GnomeListView))
+#define GNOME_LIST_VIEW_CLASS(klass)    (GTK_CHECK_CLASS_CAST ((klass),	\
+					 GNOME_TYPE_LIST_VIEW, GnomeListViewClass))
+#define GNOME_IS_LIST_VIEW(obj)         (GTK_CHECK_TYPE ((obj),		\
+					 GNOME_TYPE_LIST_VIEW))
+#define GNOME_IS_LIST_VIEW_CLASS(klass) (GTK_CHECK_CLASS_TYPE ((klass),	\
+					 GNOME_TYPE_LIST_VIEW))
+
+
+typedef struct _GnomeListView GnomeListView;
+typedef struct _GnomeListViewClass GnomeListViewClass;
+
+struct _GnomeListView {
+	GnomeCanvas canvas;
+
+	/* Private data */
+	gpointer priv;
+};
+
+struct _GnomeListViewClass {
+	GnomeCanvasClass parent_class;
+
+	void (* set_model) (GnomeListView *view, GnomeListModel *model);
+	void (* set_item_view_factory) (GnomeListView *view, GnomeItemViewFactory *factory);
+};
+
+
+GtkType gnome_list_view_get_type (void);
+
+
+
+END_GNOME_DECLS
+
+#endif
