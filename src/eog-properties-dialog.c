e027b0343a0f048b1691edde1356ad0a631ddec2
Felix Riemann <friemann@svn.gnome.org>
2008-08-21 10:13:37 +0000

    Fix segfaults with XMP-only files when opening properties dialog. Fixes
    2008-08-21  Felix Riemann  <friemann@svn.gnome.org>
    
    	* src/eog-properties-dialog.c: (eog_exif_set_focal_length_label):
    	Fix segfaults with XMP-only files when opening properties dialog.
    	Fixes bug #548738.
    
    svn path=/trunk/; revision=4698

--- src/eog-properties-dialog.c
+++ src/eog-properties-dialog.c
@@ -203,10 +203,14 @@ eog_exif_set_focal_length_label (GtkWidget *w, ExifData *exif_data)
 	gfloat f_val = 0.0;
 	gchar *fl_text = NULL,*fl35_text = NULL;
 
-	entry = exif_data_get_entry (exif_data, EXIF_TAG_FOCAL_LENGTH);
-	entry35mm = exif_data_get_entry (exif_data,
-					 EXIF_TAG_FOCAL_LENGTH_IN_35MM_FILM);
-	byte_order = exif_data_get_byte_order (exif_data);
+	/* If no ExifData is supplied the label will be
+	 * cleared later as *fl35text is NULL. */
+	if (exif_data != NULL) {
+		entry = exif_data_get_entry (exif_data, EXIF_TAG_FOCAL_LENGTH);
+		entry35mm = exif_data_get_entry (exif_data,
+					    EXIF_TAG_FOCAL_LENGTH_IN_35MM_FILM);
+		byte_order = exif_data_get_byte_order (exif_data);
+	}
 
 	if (entry && G_LIKELY (entry->format == EXIF_FORMAT_RATIONAL)) {
 		ExifRational value;
@@ -247,6 +251,7 @@ eog_exif_set_focal_length_label (GtkWidget *w, ExifData *exif_data)
 			gtk_label_set_text (GTK_LABEL (w), fl_text);
 		}
 	} else {
+		/* This will also clear the label if no ExifData was supplied */
 		gtk_label_set_text (GTK_LABEL (w), fl35_text);
 	}
 
