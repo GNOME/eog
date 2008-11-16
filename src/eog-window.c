52d355c750c841356817887de8e0b686ca3bfe79
Felix Riemann <friemann@svn.gnome.org>
2008-11-16 20:51:23 +0000

    Fix broken keyboard keys if the toolbar is hidden and no image being
    2008-11-16  Felix Riemann  <friemann@svn.gnome.org>
    
    	* src/eog-window.c: (eog_window_key_press): Fix broken keyboard
    	keys if the toolbar is hidden and no image being displayed yet.
    	Fixes bug #560396 (Frederic Peters).
    
    svn path=/trunk/; revision=4878

--- src/eog-window.c
+++ src/eog-window.c
@@ -4516,9 +4516,10 @@ eog_window_key_press (GtkWidget *widget, GdkEventKey *event)
 
 	/* If the focus is not in the toolbar and we still haven't handled the
 	   event, give the scrollview a chance to do it.  */
-	if ((tbcontainer->focus_child == NULL) && result == FALSE) {
-		result = gtk_widget_event (GTK_WIDGET (EOG_WINDOW (widget)->priv->view),
-					   (GdkEvent *) event);
+	if (tbcontainer->focus_child == NULL && result == FALSE && 
+		GTK_WIDGET_REALIZED(GTK_WIDGET (EOG_WINDOW (widget)->priv->view))) {
+			result = gtk_widget_event (GTK_WIDGET (EOG_WINDOW (widget)->priv->view),
+						   (GdkEvent *) event);
 	}
 
 	if (result == FALSE && GTK_WIDGET_CLASS (eog_window_parent_class)->key_press_event) {
