cee1f77d67767487f6cf12a01f122e2d7f11b275
Felix Riemann <friemann@gnome.org>
2013-10-30 20:58:12 +0100

    EogMetadataSidebar: Allow filename to be wrapped at char boundaries
    It is possible for this field to contain no "word".
    Enable wrapping at char boundaries as fallback so that such files
    don't cause the sidebar enlarging too much.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=700717

--- src/eog-metadata-sidebar.c
+++ src/eog-metadata-sidebar.c
@@ -504,6 +504,11 @@ eog_metadata_sidebar_init (EogMetadataSidebar *sidebar)
 	label = _gtk_grid_append_prop_line (GTK_GRID (priv->grid), label,
 					    NULL, _("Folder:"));
 
+	/* Enable wrapping at char boundaries as fallback for the filename
+	 * as it is possible for it to not contain any "words" to wrap on. */
+	gtk_label_set_line_wrap_mode (GTK_LABEL (priv->name_label),
+				      PANGO_WRAP_WORD_CHAR);
+
 {
 	priv->folder_button = gtk_button_new_with_label ("");
 	g_signal_connect (priv->folder_button, "clicked",
