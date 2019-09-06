1e8322f2cf5ffc8d136ae47dcf9750fc35192d1f
Krifa75 <yahiaoui.fakhri@gmail.com>
2019-09-06 17:42:36 +0200

    EogThumbView: Fix thumbnails not loading when re-using a window
    When switching to a folder that has the same number of images
    than the previous one in single-window mode the thumbview wouldn't
    display the thumbnails of the new folder.
    
    Part of !27.

--- src/eog-thumb-view.c
+++ src/eog-thumb-view.c
@@ -638,7 +638,6 @@ eog_thumb_view_init (EogThumbView *thumbview)
 	thumbview->priv->visible_range_changed_id = 0;
 	thumbview->priv->image_add_id = 0;
 	thumbview->priv->image_removed_id = 0;
-
 }
 
 /**
@@ -738,6 +737,7 @@ eog_thumb_view_set_model (EogThumbView *thumbview, EogListStore *store)
 	                             G_CALLBACK (eog_thumb_view_row_deleted_cb),
 	                             thumbview);
 
+	thumbview->priv->start_thumb = thumbview->priv->end_thumb = 0;
 	thumbview->priv->n_images = eog_list_store_length (store);
 
 	index = eog_list_store_get_initial_pos (store);
